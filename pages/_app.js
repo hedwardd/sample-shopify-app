8df8db0382c30c77b12242c06559fbba1de59a6d
Heath Daniel <mr.heath.daniel@gmail.com>
2021-05-03 22:16:56 -0400

    Add app-bridge, router, and more UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import { AppProvider } from '@shopify/polaris';
+import { Provider } from '@shopify/app-bridge-react';
 import '@shopify/polaris/dist/styles.css';
 import translations from '@shopify/polaris/locales/en.json';
+import ClientRouter from '../components/ClientRouter';
 
 import App from 'next/app';
 import Head from 'next/head';
 
 class MyApp extends App {
   render() {
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps, shopOrigin } = this.props;
+
+    const config = { apiKey: API_KEY, shopOrigin, forceRedirect: true };
     return (
       <React.Fragment>
         <Head>
           <title>Sample App</title>
           <meta charSet="utf-8" />
         </Head>
-        <AppProvider i18n={translations}>
-          <Component {...pageProps} />
-        </AppProvider>
+        <Provider config={config}>
+          <ClientRouter />
+          <AppProvider i18n={translations}>
+            <Component {...pageProps} />
+          </AppProvider>
+        </Provider>
       </React.Fragment>
     );
+  }
+}
+
+MyApp.getInitialProps = async ({ ctx }) => {
+  return {
+    shopOrigin: ctx.query.shop,
   }
 }
 
